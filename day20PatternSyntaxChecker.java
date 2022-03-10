97a05265f2c98a2a1693c41074f5ad4cd6dbdf29
UlfaMaulidia <99638319+UlfaMaulidia@users.noreply.github.com>
2022-03-10 20:29:54 +0700

    Add files via upload

---
+++ day20PatternSyntaxChecker.java
@@ -0,0 +1,17 @@
+public class App {
+    ppublic static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+			String patternIn = in.nextLine();
+          	//Write your code
+              try{
+                  Pattern.compile(patternIn);
+                  System.out.println("Valid");   
+              }catch(PatternSyntaxException ex){
+                  System.out.println("Invalid");
+              }
+              testCases--;
+		}
+	}
+}
